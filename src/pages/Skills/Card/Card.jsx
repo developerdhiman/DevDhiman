54b01815491495ddb45f125a027c347c6d91076e
Dhiman Chandra <prdhimanchandra@gmail.com>
2025-06-06 22:06:51 +0600

    add page some

---
+++ src/pages/Skills/Card/Card.jsx
@@ -0,0 +1,80 @@
+const Card = () => {
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 mt-10">
+      <div class="card bg-base-100 shadow-2xl w-fit">
+        <div class="card-body px-14 py-8">
+          <div class="flex items-center">
+            <h2 class="card-title text-blue-500">React.js</h2>
+          </div>
+          <progress
+            class="progress progress-primary w-full"
+            value="70"
+            max="100"
+          ></progress>
+        </div>
+      </div>
+              <div class="card bg-base-100 shadow-2xl w-fit">
+          <div class="card-body px-14 py-8">
+            <div class="flex items-center">
+              <h2 class="card-title text-blue-500">React.js</h2>
+            </div>
+            <progress
+              class="progress progress-primary w-full"
+              value="70"
+              max="100"
+            ></progress>
+          </div>
+        </div>
+                <div class="card bg-base-100 shadow-2xl w-fit">
+          <div class="card-body px-14 py-8">
+            <div class="flex items-center">
+              <h2 class="card-title text-blue-500">React.js</h2>
+            </div>
+            <progress
+              class="progress progress-primary w-full"
+              value="70"
+              max="100"
+            ></progress>
+          </div>
+        </div>
+                <div class="card bg-base-100 shadow-2xl w-fit">
+          <div class="card-body px-14 py-8">
+            <div class="flex items-center">
+              <h2 class="card-title text-blue-500">React.js</h2>
+            </div>
+            <progress
+              class="progress progress-primary w-full"
+              value="70"
+              max="100"
+            ></progress>
+          </div>
+        </div>
+                <div class="card bg-base-100 shadow-2xl w-fit">
+          <div class="card-body px-14 py-8">
+            <div class="flex items-center">
+              <h2 class="card-title text-blue-500">React.js</h2>
+            </div>
+            <progress
+              class="progress progress-primary w-full"
+              value="70"
+              max="100"
+            ></progress>
+          </div>
+        </div>
+                <div class="card bg-base-100 shadow-2xl w-fit">
+          <div class="card-body px-14 py-8">
+            <div class="flex items-center">
+              <h2 class="card-title text-blue-500">React.js</h2>
+            </div>
+            <progress
+              class="progress progress-primary w-full"
+              value="70"
+              max="100"
+            ></progress>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,21 +1,14 @@
+import Card from "./Card/card";
+
 const Skills = () => {
   return (
     <div className="px-40">
       <h1 className="text-4xl font-bold">Skills</h1>
-      <h3 className="text-2xl font-bold ml-20 border-b-2">Front-end</h3>
+      <h3 className="text-2xl font-bold border-b-2 mt-8">Front-end</h3>
+
+      {/* Front-end Card */}
       <div>
-        <div class="card bg-base-100 shadow-xl">
-          <div class="card-body">
-            <div class="flex items-center">
-              <h2 class="card-title text-blue-500">React.js</h2>
-            </div>
-            <progress
-              class="progress progress-primary w-full"
-              value="70"
-              max="100"
-            ></progress>
-          </div>
-        </div>
+        <Card></Card>
       </div>
     </div>
   );
