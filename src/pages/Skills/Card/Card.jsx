164f79c4ac3746297c0d25bef872e4e6a273eb2a
Dhiman Chandra <prdhimanchandra@gmail.com>
2025-06-06 22:54:19 +0600

    icon setup

--- src/pages/Skills/Card/Card.jsx
+++ src/pages/Skills/Card/Card.jsx
@@ -1,10 +1,19 @@
+import { FaReact, FaVuejs } from "react-icons/fa";
+import {
+  MdOutlineCss,
+  MdOutlineHtml,
+  MdOutlineJavascript,
+} from "react-icons/md";
+import { RiAngularjsLine } from "react-icons/ri";
+
 const Card = () => {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 mt-10">
-      <div class="card bg-base-100 shadow-2xl w-fit">
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
+      <div class="card bg-base-100 shadow-2xl w-full">
         <div class="card-body px-14 py-8">
-          <div class="flex items-center">
-            <h2 class="card-title text-blue-500">React.js</h2>
+          <div class="flex flex-col items-center">
+            <MdOutlineHtml className="text-4xl text-orange-600" />
+            <h2 class="card-title text-blue-500">HTML5</h2>
           </div>
           <progress
             class="progress progress-primary w-full"
@@ -13,66 +22,71 @@ const Card = () => {
           ></progress>
         </div>
       </div>
-              <div class="card bg-base-100 shadow-2xl w-fit">
-          <div class="card-body px-14 py-8">
-            <div class="flex items-center">
-              <h2 class="card-title text-blue-500">React.js</h2>
-            </div>
-            <progress
-              class="progress progress-primary w-full"
-              value="70"
-              max="100"
-            ></progress>
+      <div class="card bg-base-100 shadow-2xl w-full">
+        <div class="card-body px-14 py-8">
+          <div class="flex flex-col items-center">
+            <MdOutlineCss className="text-4xl text-sky-600" />
+            <h2 class="card-title text-blue-500">CSS3</h2>
           </div>
+          <progress
+            class="progress progress-primary w-full"
+            value="70"
+            max="100"
+          ></progress>
         </div>
-                <div class="card bg-base-100 shadow-2xl w-fit">
-          <div class="card-body px-14 py-8">
-            <div class="flex items-center">
-              <h2 class="card-title text-blue-500">React.js</h2>
-            </div>
-            <progress
-              class="progress progress-primary w-full"
-              value="70"
-              max="100"
-            ></progress>
+      </div>
+      <div class="card bg-base-100 shadow-2xl w-full">
+        <div class="card-body px-14 py-8">
+          <div class="flex flex-col items-center">
+            <MdOutlineJavascript className="text-4xl text-orange-400" />
+            <h2 class="card-title text-blue-500">JavaScript</h2>
           </div>
+          <progress
+            class="progress progress-primary w-full"
+            value="70"
+            max="100"
+          ></progress>
         </div>
-                <div class="card bg-base-100 shadow-2xl w-fit">
-          <div class="card-body px-14 py-8">
-            <div class="flex items-center">
-              <h2 class="card-title text-blue-500">React.js</h2>
-            </div>
-            <progress
-              class="progress progress-primary w-full"
-              value="70"
-              max="100"
-            ></progress>
+      </div>
+      <div class="card bg-base-100 shadow-2xl w-full">
+        <div class="card-body px-14 py-8">
+          <div class="flex flex-col items-center">
+            <FaReact className="text-2xl text-blue-500" />
+            <h2 class="card-title">React.js</h2>
           </div>
+          <progress
+            class="progress progress-primary w-full"
+            value="70"
+            max="100"
+          ></progress>
         </div>
-                <div class="card bg-base-100 shadow-2xl w-fit">
-          <div class="card-body px-14 py-8">
-            <div class="flex items-center">
-              <h2 class="card-title text-blue-500">React.js</h2>
-            </div>
-            <progress
-              class="progress progress-primary w-full"
-              value="70"
-              max="100"
-            ></progress>
+      </div>
+      <div class="card bg-base-100 shadow-2xl w-full">
+        <div class="card-body px-14 py-8">
+          <div class="flex flex-col items-center">
+            <FaVuejs className="text-2xl text-green-600" />
+            <h2 class="card-title">Vue.js</h2>
           </div>
+          <progress
+            class="progress progress-primary w-full"
+            value="70"
+            max="100"
+          ></progress>
         </div>
-                <div class="card bg-base-100 shadow-2xl w-fit">
-          <div class="card-body px-14 py-8">
-            <div class="flex items-center">
-              <h2 class="card-title text-blue-500">React.js</h2>
-            </div>
-            <progress
-              class="progress progress-primary w-full"
-              value="70"
-              max="100"
-            ></progress>
+      </div>
+      <div class="card bg-base-100 shadow-2xl w-full">
+        <div class="card-body px-14 py-8">
+          <div class="flex flex-col items-center">
+            <RiAngularjsLine className="text-3xl text-orange-600"/>
+            <h2 class="card-title">Angular</h2>
           </div>
+          <progress
+            class="progress progress-primary w-full"
+            value="70"
+            max="100"
+          ></progress>
         </div>
+      </div>
     </div>
   );
 };
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import Card from "./Card/card";
 
 const Skills = () => {
   return (
-    <div className="px-40">
+    <div className="px-40 bg-gray-100">
       <h1 className="text-4xl font-bold">Skills</h1>
       <h3 className="text-2xl font-bold border-b-2 mt-8">Front-end</h3>
 
